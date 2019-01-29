b59bc56a431572ef5d2bfc70fb7339c83a2510a7
Adrien Grosjean <grosjeanadrien@gmail.com>
2019-01-29 19:54:54 +0100

    Documentation update

---
+++ docs/files_dup.js
@@ -0,0 +1,23 @@
+var files_dup =
+[
+    [ "BRDFMapPoint.h", "_b_r_d_f_map_point_8h.html", [
+      [ "BRDFMapPoint", "class_chef_devr_1_1_b_r_d_f_map_point.html", "class_chef_devr_1_1_b_r_d_f_map_point" ]
+    ] ],
+    [ "BRDFMapScene.cpp", "_b_r_d_f_map_scene_8cpp.html", null ],
+    [ "BRDFMapScene.h", "_b_r_d_f_map_scene_8h.html", [
+      [ "BRDFMapScene", "class_chef_devr_1_1_b_r_d_f_map_scene.html", "class_chef_devr_1_1_b_r_d_f_map_scene" ]
+    ] ],
+    [ "BRDFReconstructed.cpp", "_b_r_d_f_reconstructed_8cpp.html", null ],
+    [ "BRDFReconstructed.h", "_b_r_d_f_reconstructed_8h.html", [
+      [ "BRDFReconstructed", "class_chef_devr_1_1_b_r_d_f_reconstructed.html", "class_chef_devr_1_1_b_r_d_f_reconstructed" ]
+    ] ],
+    [ "BRDFReconstructionModel.cpp", "_b_r_d_f_reconstruction_model_8cpp.html", null ],
+    [ "BRDFReconstructionModel.h", "_b_r_d_f_reconstruction_model_8h.html", [
+      [ "BRDFReconstructionModel", "class_chef_devr_1_1_b_r_d_f_reconstruction_model.html", "class_chef_devr_1_1_b_r_d_f_reconstruction_model" ]
+    ] ],
+    [ "ParameterWindow.h", "_parameter_window_8h.html", [
+      [ "ParameterWindow", "class_parameter_window.html", "class_parameter_window" ]
+    ] ],
+    [ "Parametrisation.h", "_parametrisation_8h.html", "_parametrisation_8h" ],
+    [ "types.h", "types_8h.html", "types_8h" ]
+];
---
+++ docs/namespaces_dup.js
@@ -0,0 +1,4 @@
+var namespaces_dup =
+[
+    [ "ChefDevr", "namespace_chef_devr.html", null ]
+];
