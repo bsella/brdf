9569f6d8acf13046e6e6288aa1edc46bf8aef4c6
burg ludovic <brl1547a@u3-112-17l.ens.fsi>
2019-02-06 13:38:33 +0100

    BRDFReconstructionModel constructor done.

--- src/brdf/ChefDevr/BRDFReconstructionModel.h
+++ src/brdf/ChefDevr/BRDFReconstructionModel.h
@@ -9,6 +9,10 @@
 
 #include "BRDFReconstructed.h"
 #include "Parametrisation.h"
+#include "types.h"
+
+#include <iostream>
+#include <fstream>
 
 namespace ChefDevr
 {
@@ -29,8 +33,8 @@ namespace ChefDevr
          * 
          * Loads K_minus1, X and Z from parametrisation data file
          */
-        BRDFReconstructionModel(std::string& paramtrzFilePath, std::string& brdfFolderPath);
-        ~BRDFReconstructionModel();
+        BRDFReconstructionModel(std::string paramtrzFilePath, std::string brdfFolderPath);
+        ~BRDFReconstructionModel() {}
         
         /**
          * @brief Constructs a BRDF from its latent coordinates
@@ -39,6 +43,12 @@ namespace ChefDevr
          * @return the reconstructed BRDF
          */
         BRDFReconstructed createBRDFFromLSCoord (Scalar x, Scalar y);
+
+        class BRDFReconstructionModelError : public std::runtime_error {
+        public:
+            explicit BRDFReconstructionModelError(const std::string &message_error) :
+            runtime_error{message_error} {}
+        };
 
     private:
         /**
@@ -77,13 +87,18 @@ namespace ChefDevr
         /** 
          * @brief Dimension of the latent space
          */
-        const unsigned int dim;
+        unsigned int dim;
         
         /**
          * @brief The object that does BRDF reconstruction computations
          */
         //BRDFReconstructor brdfReconstructor;
+
+        RowVector<Scalar> read_brdf(unsigned int num_coefficientsNeeded, const char *filePath);
+
     };
 }// namespace ChefDevr
+
+#include "BRDFReconstructionModel.hpp"
 
 #endif
